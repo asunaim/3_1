d2571f7eb61e8a48d5aa73a8b4698209f8040832
michelle19l <48280227+michelle19l@users.noreply.github.com>
2020-12-10 14:41:22 +0800

    Add files via upload

---
+++ 网络/3-2/server/class.h
@@ -0,0 +1,237 @@
+#pragma once
+#pragma once
+# define _CRT_SECURE_NO_WARNINGS
+# define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib,"ws2_32.lib")//链接ws2_32.lib库文件到此项目中
+using namespace std;
+#include <iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include <WinSock2.h>//windows socket 编程头文件
+#include<string.h>
+#include<cstring>
+#include<string>
+#include<ctime>
+#include <fstream>
+
+clock_t clockstart, clockend;
+int sendseq = 0;
+int recvseq = 0;
+
+#define SPORT 30000
+#define CPORT 6666
+#define BUF_SIZE 1024
+#define WAIT_TIME 5//1s
+#define SENT_TIMES 8//最多八次重传
+#define FILE_PACKET_LENGTH 1024
+//线程
+
+
+
+int addr_len = sizeof(struct sockaddr_in);
+SOCKET sock;
+SOCKADDR_IN addrop, addr;//ip+端口号
+char content[10000][1024];
+int base, sendnextseq, recvnextseq;
+int  N = 10;
+
+
+#pragma pack(1)
+struct message//报文格式
+{
+	//ACK=0x01, SYN=0x02, FIN=0x04, NAK=0x08,EXIST 0x10,startfile 0x20,endfile 0x40
+	int flag;//标志位
+	DWORD SendIP, RecvIP;
+	int msgseq;
+	int length;
+	int SendPort, RecvPort;
+	int ackseq;
+	//int synseq;
+	//int finseq;
+	//int nakseq;//消息序号和确认的消息序号
+	int index;
+	int filelength;
+	u_short checksum;
+	char msg[BUF_SIZE];
+	message();
+	void set_send_ip(char* s);
+	void set_recv_ip(char* s);
+	void set_send_port(int s);
+	void set_recv_port(int s);
+	void set_ack();
+	void set_syn();
+	void set_fin();
+	void set_nak();
+	void set_exist();
+	void set_startfile();
+	void set_endfile();
+
+	int get_ack();
+	int get_syn();
+	int get_fin();
+	int get_nak();
+	int get_exist();
+	int get_startfile();
+	int get_endfile();
+
+	void setchecksum();//设置校验和
+	bool checkchecksum();//检验校验和
+
+	void print();//输出标志位
+};
+#pragma pack()
+
+struct filepacket//拷贝文件内容时需要的信息
+{
+	message a;
+	int index;
+	int length;
+	filepacket()
+	{
+		memset((char*)this, 0, sizeof(filepacket));
+	}
+};
+
+
+
+message::message() {
+	memset(this, 0, sizeof(message));//初始化全0
+}
+
+void message::set_send_ip(char* s) {}
+void message::set_recv_ip(char* s) {}
+void message::set_send_port(int s) {}
+void message::set_recv_port(int s) {}
+//ACK=0x01, SYN=0x02, FIN=0x04, NAK=0x08
+void message::set_ack()
+{
+	if (get_ack() == 0)
+		flag += 0x01;
+}
+void message::set_syn()
+{
+	if (get_syn() == 0)
+		flag += 0x02;
+}
+void message::set_fin()
+{
+	if (get_fin() == 0)
+		flag += 0x04;
+}
+void message::set_nak()
+{
+	if (get_nak() == 0)
+		flag += 0x08;
+}
+void message::set_exist()
+{
+	if (get_exist() == 0)
+		flag += 0x10;
+}
+
+void message::set_startfile()
+{
+	if (get_startfile() == 0)
+		flag += 0x20;
+}
+void message::set_endfile()
+{
+	if (get_startfile() == 0)
+		flag += 0x40;
+}
+
+int message::get_ack()
+{
+	if (this->flag & 0x01)
+		return 1;
+	else return 0;
+}
+int message::get_syn()
+{
+	if (this->flag & 0x02)
+		return 1;
+	else return 0;
+}
+int message::get_fin()
+{
+	if (this->flag & 0x04)
+		return 1;
+	else return 0;
+}
+int message::get_nak()
+{
+	if (this->flag & 0x08)
+		return 1;
+	else return 0;
+}
+int message::get_exist()
+{
+	if (this->flag & 0x10)
+		return 1;
+	else return 0;
+}
+int message::get_startfile()
+{
+	if (this->flag & 0x20)
+		return 1;
+	else return 0;
+}
+int message::get_endfile()
+{
+	int t = this->flag & 0x40;
+	if (t)
+		return 1;
+	else return 0;
+}
+
+void message::print()//输出标志位
+{
+	cout << msgseq << " ";
+	if (get_ack()) { cout << "ACK "; cout << ackseq << " "; }
+	if (get_syn())cout << "SYN ";
+	if (get_fin())cout << "FIN ";
+	if (get_nak())cout << "NAK ";
+	if (get_exist())cout << "EX ";
+	if (get_startfile())cout << "SF ";
+	if (get_endfile())cout << "EF ";
+	cout << endl;
+}
+
+void message::setchecksum()
+{
+	int sum = 0;
+	u_char* temp = (u_char*)this;
+	for (int i = 0; i < 16; i++)
+	{
+		sum += temp[2 * i] << 8 + temp[2 * i + 1];
+		while (sum >= 0x10000)
+		{//溢出
+			int t = sum >> 16;
+			sum += t;
+		}
+	}
+	u_short a = (u_short)sum;
+	this->checksum = ~(u_short)sum;
+	u_short t = a + ~sum;
+}
+
+bool message::checkchecksum()
+{
+	int sum = 0;
+	u_char* temp = (u_char*)this;
+	for (int i = 0; i < 16; i++)
+	{
+		sum += temp[2 * i] << 8 + temp[2 * i + 1];
+		while (sum >= 0x10000)
+		{//溢出
+			int t = sum >> 16;
+			sum += t;
+		}
+	}
+	if (checksum + (u_short)sum == 65535)
+		return true;
+	return false;
+}
+
+
+
---
+++ 网络/3-2/server/function.h
@@ -0,0 +1,107 @@
+#pragma once
+#include "class.h"
+
+
+void simplesend(message& a);//约定好各项信息之后的简易发送函数
+void simplerecv(message& a);
+
+bool stopwaitsend(message& a, message b);//停等机制
+bool stopwaitrecv(message& a, message b);
+
+
+void simplesend(message& a)
+{
+	a.set_exist();
+	a.setchecksum();
+	//a.checkchecksum();
+	if (sendto(sock, (char*)&a, sizeof(message), 0, (struct sockaddr*)&addr, sizeof(sockaddr)) == SOCKET_ERROR)
+	{
+		/*cout << "发送失败" << endl;
+		DWORD dwError = WSAGetLastError();
+		cout << dwError << endl;*/
+	}
+	else if (a.flag) { cout << "发送 "; a.print(); }
+	//recvnextseq++;
+}
+
+void simplerecv(message& a)
+{
+	memset(a.msg, 0, sizeof(a.msg));
+	recvfrom(sock, (char*)&a, sizeof(message), 0, (struct sockaddr*)&addr, &addr_len);
+	if (a.flag) { cout << "接收 "; a.print(); }
+}
+
+
+bool check(message a, message b)
+{
+	/*if (b.get_ack() == 1 && b.ackseq == a.msgseq)
+		return 1;
+	else return 0;*/
+	return 1;
+}
+
+
+bool stopwaitsend(message& a, message b)//a写入待发送消息，如果收到对方返回的ack则成功
+{
+	simplesend(a);
+	clockstart = clock();
+	int flag = 0;//重发超过10次退出
+	while (1)
+	{
+		simplerecv(b);
+		if (b.get_ack()&& b.checkchecksum())//b包含对消息a的ack
+		{
+			cout << "发送&ack" << endl;
+			return 1;
+		}
+		clockend = clock();
+		if (flag == SENT_TIMES)
+			return 0;
+		if ((clockend - clockstart) / CLOCKS_PER_SEC >= WAIT_TIME)//超时
+		{
+			flag++;
+			clockstart = clock();
+			cout << "重传" << flag << endl;
+			simplesend(a);//重传
+		}
+	}
+	return 0;//0代表发送失败
+}
+
+
+
+bool stopwaitrecv(message& a, message b)//收到的消息写入a中
+{
+	int flag = 0;
+	while (1)
+	{
+		simplerecv(a);
+		if (a.get_exist())
+		{
+			int check = a.checkchecksum();
+			if(check)
+			{
+				b.set_ack();
+				simplesend(b);
+				memset((char*)&b, 0, sizeof(message));
+				cout << "接收&ack" << endl;
+				return 1;
+			}
+		}
+	}
+	cout << "接收失败" << endl;
+	return 0;
+}
+
+void time()
+{
+	clock_t start, end;
+	start = clock();		//程序开始计时
+	int ans = 0;
+	for (int i = 1; i <= 1e8; i++)
+		ans++;
+	end = clock();		//程序结束用时
+	double endtime = (double)(end - start) / CLOCKS_PER_SEC;
+	cout << "Total time:" << endtime << endl;		//s为单位
+	cout << "Total time:" << endtime * 1000 << "ms" << endl;	//ms为单位
+}
